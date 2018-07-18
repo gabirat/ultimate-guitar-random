8aa894cbb23182ecab86ed19019131bf7279b577
gabirat <gabirat@onet.pl>
2018-07-18 02:51:27 +0200

    corrected path

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const get_random_song = require("./ug-get-random-song");
 const app = require("express")();
 
-app.get("/random", async (req, res) => {
+app.get("/random-song", async (req, res) => {
     res.set("Access-Control-Allow-Origin", "http://gabirat.pl");
     res.send(await get_random_song(req.query.type, false, req.query.order));
 });
